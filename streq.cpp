2c64f45df129a9b378f03260cd60f2cccb6c47e0
Stephanie Lee <134336879+stephanie-1427@users.noreply.github.com>
2026-02-02 14:25:06 -0700

    Update streq.cpp

--- streq.cpp
+++ streq.cpp
@@ -1,8 +1,3 @@
-// write bool streq(const char * lhs, const char * rhs)
-// using no helper functions, no temp variables, and only one loop
-// should return true if the given strings are equal (same length and characters)
-// nullptr is a possible value for the parameters
-
 bool streq(const char* lhs, const char* rhs) {
     if (lhs == rhs) { return true; } // if both are same address or nullptr
     if (!lhs || !rhs) { return false; } // if one side is nullptr
